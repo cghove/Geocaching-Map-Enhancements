5c190d06ad2e553c1fdae725efe78bb38bec2082
cghove <26240443+cghove@users.noreply.github.com>
2021-04-06 21:32:40 +0200

    Add files via upload

---
+++ GeocachingMapEnhancements.meta.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        Geocaching Map Enhancements
+// @version     0.8.3
+// @author      cghove
+// @oujs:author JRI
+// @namespace   inge.org.uk/userscripts
+// @description Adds extra maps and grid reference search to Geocaching.com, along with several other enhancements.
+// @include     https://www.geocaching.com/*
+// @license     MIT; http://www.opensource.org/licenses/mit-license.php
+// @attribution   2011-18, James Inge (http://geo.inge.org.uk/)
+// @attribution GeoNames (http://www.geonames.org/)
+// @attribution Postcodes.io (https://postcodes.io/)
+// @attribution Chris Veness (http://www.movable-type.co.uk/scripts/latlong-gridref.html)
+// @grant       GM_xmlhttpRequest
+// @grant       GM.xmlHttpRequest
+// @connect     geograph.org.uk
+// @connect     channel-islands.geographs.org
+// @connect     geo-en.hlipp.de
+// @connect     api.geonames.org
+// @connect     api.postcodes.io
+// @connect     www.geocaching.com
+// @icon        https://geo.inge.org.uk/userscripts/GeocachingMap48.png
+// @icon64      https://geo.inge.org.uk/userscripts/GeocachingMap64.png
+// @updateURL   https://geo.inge.org.uk/userscripts/GeocachingMapEnhancements.meta.js
+// @downloadURL https://openuserjs.org/install/JRI/Geocaching_Map_Enhancements.user.js
+// ==/UserScript==
